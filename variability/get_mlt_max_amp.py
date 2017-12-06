5cd0734e8599e54b400bf118d059f8e32bf2397f
danielsf <scott.f.daniel@gmail.com>
2017-12-06 09:40:10 -0800

    add ebv dust lookup to get_mlt_max_amp.py

--- variability/get_mlt_max_amp.py
+++ variability/get_mlt_max_amp.py
@@ -10,9 +10,60 @@ from lsst.utils import getPackageDir
 from lsst.sims.catalogs.db import DBObject
 from lsst.sims.photUtils import Sed
 from lsst.sims.utils import radiansFromArcsec
+from lsst.sims.photUtils import BandpassDict
+
+
+def get_mlt_grid():
+
+    # Construct a look-up table to determine the factor
+    # by which to multiply the flares' flux to account for
+    # dust as a function of E(B-V).  Recall that we are
+    # modeling all MLT flares as 9000K blackbodies.
+
+    lsstBandpassDict = BandpassDict.loadTotalBandpassesFromFiles()
+
+    ebv_grid = np.arange(0.0, 7.01, 0.01)
+    bb_wavelen = np.arange(200.0, 1500.0, 0.1)
+    hc_over_k = 1.4387e7  # nm*K
+    temp = 9000.0  # black body temperature in Kelvin
+    exp_arg = hc_over_k/(temp*bb_wavelen)
+    exp_term = 1.0/(np.exp(exp_arg) - 1.0)
+    ln_exp_term = np.log(exp_term)
+
+    # Blackbody f_lambda function;
+    # discard normalizing factors; we only care about finding the
+    # ratio of fluxes between the case with dust extinction and
+    # the case without dust extinction
+    log_bb_flambda = -5.0*np.log(bb_wavelen) + ln_exp_term
+    bb_flambda = np.exp(log_bb_flambda)
+    bb_sed = Sed(wavelen=bb_wavelen, flambda=bb_flambda)
+
+    base_fluxes = lsstBandpassDict.fluxListForSed(bb_sed)
+
+    a_x, b_x = bb_sed.setupCCMab()
+    _mlt_dust_lookup = {}
+    _mlt_dust_lookup['ebv'] = ebv_grid
+    list_of_bp = lsstBandpassDict.keys()
+    for bp in list_of_bp:
+        _mlt_dust_lookup[bp] = np.zeros(len(ebv_grid))
+    for iebv, ebv_val in enumerate(ebv_grid):
+        wv, fl = bb_sed.addCCMDust(a_x, b_x,
+                                   ebv=ebv_val,
+                                   wavelen=bb_wavelen,
+                                   flambda=bb_flambda)
+
+        dusty_bb = Sed(wavelen=wv, flambda=fl)
+        dusty_fluxes = lsstBandpassDict.fluxListForSed(dusty_bb)
+        for ibp, bp in enumerate(list_of_bp):
+            _mlt_dust_lookup[bp][iebv] = dusty_fluxes[ibp]/base_fluxes[ibp]
+
+    return _mlt_dust_lookup
+
 
 
 if __name__ == "__main__":
+
+    mlt_grid = get_mlt_grid()
 
     lc_data_dir = os.path.join(getPackageDir('sims_data'),
                                'catUtilsData')
@@ -40,11 +91,12 @@ if __name__ == "__main__":
                     ('umag', float), ('gmag', float), ('rmag', float),
                     ('imag', float), ('zmag', float), ('ymag', float),
                     ('sdssr', float),
-                    ('varParamStr', str, 200), ('parallax', float)])
+                    ('varParamStr', str, 200), ('parallax', float),
+                    ('ebv', float)])
 
     query = 'SELECT TOP 1000000'
     query += 'htmid, simobjid, umag, gmag, rmag, imag, zmag, ymag, sdssr, '
-    query += 'varParamStr, parallax '
+    query += 'varParamStr, parallax, ebv '
     query += 'FROM stars_mlt_part_%s' % table_tag
 
     db = DBObject(database='LSSTCATSIM', host='fatboy.phys.washington.edu',
