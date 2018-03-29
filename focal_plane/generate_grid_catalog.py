d0fbb683166baec291cf90d99edf45be5f31dcb3
danielsf <scott.f.daniel@gmail.com>
2018-03-29 13:58:17 -0700

    output several catalogs with different rotSkyPos

--- focal_plane/generate_grid_catalog.py
+++ focal_plane/generate_grid_catalog.py
@@ -12,6 +12,7 @@ from lsst.sims.utils import Site
 from lsst.afw.cameraGeom import SCIENCE
 from lsst.sims.utils.CodeUtilities import _validate_inputs
 from lsst.sims.utils import arcsecFromRadians
+from lsst.sims.utils import getRotTelPos
 import palpy
 
 import argparse
@@ -169,68 +170,79 @@ if __name__ == "__main__":
     assert np.abs(obs.site.pressure)<1.0e-6
     assert np.abs(obs.site.humidity)<1.0e-6
 
-    x_pix_arr = np.arange(700.0, 3301.0, 700.0)
-    y_pix_arr = np.arange(700.0, 3301.0, 700.0)
-    pix_grid = np.meshgrid(x_pix_arr, y_pix_arr)
-    x_pix_arr = pix_grid[0].flatten()
-    y_pix_arr = pix_grid[1].flatten()
+    for i_rot, rot in enumerate((0.0, 37.0, 74.0)):
 
-    x_mm = []
-    y_mm = []
-    x_pix = []
-    y_pix = []
+        obs.rotSkyPos = rot
 
-    camera = lsst_camera()
-    det_name_list = []
-    for det in camera:
-        if det.getType() == SCIENCE:
-            det_name_list.append(det.getName())
+        x_pix_arr = np.arange(700.0, 3301.0, 700.0)
+        y_pix_arr = np.arange(700.0, 3301.0, 700.0)
+        pix_grid = np.meshgrid(x_pix_arr, y_pix_arr)
+        x_pix_arr = pix_grid[0].flatten()
+        y_pix_arr = pix_grid[1].flatten()
 
-    det_name_list.sort()
-    for det_name in det_name_list:
-        if args.chip is not None and args.chip != det_name:
-            continue
-        for xpix, ypix in zip(x_pix_arr, y_pix_arr):
-            xmm, ymm = coord_converter.mmFromPix(xpix, ypix, det_name)
-            x_mm.append(xmm)
-            y_mm.append(ymm)
-            x_pix.append(xpix)
-            y_pix.append(ypix)
+        x_mm = []
+        y_mm = []
+        x_pix = []
+        y_pix = []
 
-    x_mm = np.array(x_mm)
-    y_mm = np.array(y_mm)
-    x_pix = np.array(x_pix)
-    y_pix = np.array(y_pix)
-    id_grid = np.arange(len(x_mm)).astype(int)+1
+        camera = lsst_camera()
+        det_name_list = []
+        for det in camera:
+            if det.getType() == SCIENCE:
+                det_name_list.append(det.getName())
 
-    x_pup_arr, y_pup_arr = pupilCoordsFromFocalPlaneCoords(x_mm, y_mm, camera=lsst_camera())
+        det_name_list.sort()
+        for det_name in det_name_list:
+            if args.chip is not None and args.chip != det_name:
+                continue
+            for xpix, ypix in zip(x_pix_arr, y_pix_arr):
+                xmm, ymm = coord_converter.mmFromPix(xpix, ypix, det_name)
+                x_mm.append(xmm)
+                y_mm.append(ymm)
+                x_pix.append(xpix)
+                y_pix.append(ypix)
 
-    ra_rad, dec_rad = _rawObservedFromPupilCoords(x_pup_arr, y_pup_arr,
-                                                  obs._pointingRA,
-                                                  obs._pointingDec,
-                                                  obs._rotSkyPos)
+        x_mm = np.array(x_mm)
+        y_mm = np.array(y_mm)
+        x_pix = np.array(x_pix)
+        y_pix = np.array(y_pix)
+        id_grid = np.arange(len(x_mm)).astype(int)+1
 
-    ra_grid = np.degrees(ra_rad)
-    dec_grid = np.degrees(dec_rad)
+        x_pup_arr, y_pup_arr = pupilCoordsFromFocalPlaneCoords(x_mm, y_mm, camera=lsst_camera())
 
-    phosim_header_map = copy.deepcopy(DefaultPhoSimHeaderMap)
-    phosim_header_map['rawSeeing'] = ('rawSeeing', None)
-    phosim_header_map['FWHMeff'] = ('FWHMeff', None)
-    phosim_header_map['FWHMgeom'] = ('FWHMgeom',None)
-    phosim_header_map['nsnap'] = 1
-    phosim_header_map['vistime'] =30.0
+        ra_rad, dec_rad = _rawObservedFromPupilCoords(x_pup_arr, y_pup_arr,
+                                                      obs._pointingRA,
+                                                      obs._pointingDec,
+                                                      obs._rotSkyPos)
 
-    with open(os.path.join(args.out_dir,'star_grid_%d.txt' % (args.obs)), 'w') as out_file:
-        write_phoSim_header(obs, out_file, phosim_header_map)
-        for (i_obj, ra, dec) in zip(id_grid, ra_grid, dec_grid):
-            out_file.write('object %d ' % (i_obj))
-            out_file.write('%.17f %.17f ' % (ra, dec))
-            out_file.write('21.0 flatSED/sed_flat_short.txt.gz 0 0 0 0 0 0 point none CCM 0.03380581 3.1\n')
+        ra_grid = np.degrees(ra_rad)
+        dec_grid = np.degrees(dec_rad)
 
-    with open(os.path.join(args.out_dir, 'star_predicted_%d.txt' % (args.obs)), 'w') as out_file:
-        out_file.write('# id xmm ymm xpix ypix\n')
-        for ii in range(len(x_mm)):
-            out_file.write('%d %.17e %.17e %.6f %.6f\n' %
-                           (id_grid[ii],
-                            x_mm[ii], y_mm[ii],
-                            x_pix[ii], y_pix[ii]))
+        phosim_header_map = copy.deepcopy(DefaultPhoSimHeaderMap)
+        phosim_header_map['rawSeeing'] = ('rawSeeing', None)
+        phosim_header_map['FWHMeff'] = ('FWHMeff', None)
+        phosim_header_map['FWHMgeom'] = ('FWHMgeom',None)
+        phosim_header_map['nsnap'] = 1
+        phosim_header_map['vistime'] =30.0
+
+        phosim_header_map['rottelpos'] = getRotTelPos(obs.pointingRA,
+                                                      obs.pointingDec,
+                                                      obs,
+                                                      obs.rotSkyPos)
+
+        phosim_header_map['obshistid'] = i_rot+1
+
+        with open(os.path.join(args.out_dir,'star_grid_%d.txt' % (i_rot+1)), 'w') as out_file:
+            write_phoSim_header(obs, out_file, phosim_header_map)
+            for (i_obj, ra, dec) in zip(id_grid, ra_grid, dec_grid):
+                out_file.write('object %d ' % (i_obj))
+                out_file.write('%.17f %.17f ' % (ra, dec))
+                out_file.write('21.0 flatSED/sed_flat_short.txt.gz 0 0 0 0 0 0 point none CCM 0.03380581 3.1\n')
+
+        with open(os.path.join(args.out_dir, 'star_predicted_%d.txt' % (i_rot+1)), 'w') as out_file:
+            out_file.write('# id xmm ymm xpix ypix\n')
+            for ii in range(len(x_mm)):
+                out_file.write('%d %.17e %.17e %.6f %.6f\n' %
+                               (id_grid[ii],
+                                x_mm[ii], y_mm[ii],
+                                x_pix[ii], y_pix[ii]))
