3abf080836cadaf391b858d6faa871ebe5e0eab1
danielsf <scott.f.daniel@gmail.com>
2018-03-30 14:13:17 -0700

    move coordinate transformations into PhoSimTransform module

--- focal_plane/generate_grid_catalog.py
+++ focal_plane/generate_grid_catalog.py
@@ -2,6 +2,8 @@ import os
 import copy
 import numpy as np
 from PhoSimTransform import PhoSimPixelTransformer
+from PhoSimTransform import _rawPupilCoordsFromObserved
+from PhoSimTransform import _rawObservedFromPupilCoords
 from lsst.sims.catUtils.utils import ObservationMetaDataGenerator
 from lsst.sims.catUtils.exampleCatalogDefinitions import DefaultPhoSimHeaderMap
 from lsst.sims.catUtils.exampleCatalogDefinitions import write_phoSim_header
@@ -15,127 +17,6 @@ from lsst.sims.utils import arcsecFromRadians
 import palpy
 
 import argparse
-
-def _rawPupilCoordsFromObserved(ra_obs, dec_obs, ra0, dec0, rotSkyPos):
-    """
-    Convert Observed RA, Dec into pupil coordinates
-
-    Parameters
-    ----------
-    ra_obs is the observed RA in radians
-
-    dec_obs is the observed Dec in radians
-
-    obs_metadata is an ObservationMetaData characterizing the telescope location and pointing
-
-    epoch is the epoch of the mean RA and Dec in julian years (default=2000.0)
-
-    includeRefraction is a boolean controlling the application of refraction.
-
-    Returns
-    --------
-    A numpy array whose first row is the x coordinate on the pupil in
-    radians and whose second row is the y coordinate in radians
-    """
-
-    are_arrays = _validate_inputs([ra_obs, dec_obs], ['ra_obs', 'dec_obs'],
-                                  "pupilCoordsFromObserved")
-
-    theta = -1.0*rotSkyPos
-
-    ra_pointing = ra0
-    dec_pointing = dec0
-
-    # palpy.ds2tp performs the gnomonic projection on ra_in and dec_in
-    # with a tangent point at (pointingRA, pointingDec)
-    #
-    if not are_arrays:
-        try:
-            x, y = palpy.ds2tp(ra_obs, dec_obs, ra_pointing, dec_pointing)
-        except:
-            x = np.NaN
-            y = np.NaN
-    else:
-        try:
-            x, y = palpy.ds2tpVector(ra_obs, dec_obs, ra_pointing, dec_pointing)
-        except:
-            # apparently, one of your ra/dec values was improper; we will have to do this
-            # element-wise, putting NaN in the place of the bad values
-            x = []
-            y = []
-            for rr, dd in zip(ra_obs, dec_obs):
-                try:
-                    xx, yy = palpy.ds2tp(rr, dd, ra_pointing, dec_pointing)
-                except:
-                    xx = np.NaN
-                    yy = np.NaN
-                x.append(xx)
-                y.append(yy)
-            x = np.array(x)
-            y = np.array(y)
-
-    # rotate the result by rotskypos (rotskypos being "the angle of the sky relative to
-    # camera coordinates" according to phoSim documentation) to account for
-    # the rotation of the focal plane about the telescope pointing
-
-    x_out = x*np.cos(theta) - y*np.sin(theta)
-    y_out = x*np.sin(theta) + y*np.cos(theta)
-
-    return np.array([x_out, y_out])
-
-def _rawObservedFromPupilCoords(xPupil, yPupil, ra0, dec0, rotSkyPos):
-    """
-    Convert pupil coordinates into observed (RA, Dec)
-
-    @param [in] xPupil -- pupil coordinates in radians.
-    Can be a numpy array or a number.
-
-    @param [in] yPupil -- pupil coordinates in radians.
-    Can be a numpy array or a number.
-
-    @param [in] obs_metadata -- an instantiation of ObservationMetaData characterizing
-    the state of the telescope
-
-    @param [in] epoch -- julian epoch of the mean equinox used for the coordinate
-    transformations (in years; defaults to 2000)
-
-    @param[in] includeRefraction -- a boolean which controls the effects of refraction
-    (refraction is used when finding the observed coordinates of the boresite specified
-    by obs_metadata)
-
-    @param [out] a 2-D numpy array in which the first row is observed RA and the second
-    row is observed Dec (both in radians).  Note: these are not ICRS coordinates.
-    These are RA and Dec-like coordinates resulting from applying precession, nutation,
-    diurnal aberration and annual aberration on top of ICRS coordinates.
-
-    WARNING: This method does not account for apparent motion due to parallax.
-    This method is only useful for mapping positions on a theoretical focal plane
-    to positions on the celestial sphere.
-    """
-
-    are_arrays = _validate_inputs([xPupil, yPupil], ['xPupil', 'yPupil'],
-                                  "observedFromPupilCoords")
-
-    ra_pointing = ra0
-    dec_pointing = dec0
-
-    # This is the same as theta in pupilCoordsFromRaDec, except without the minus sign.
-    # This is because we will be reversing the rotation performed in that other method.
-    theta = rotSkyPos
-
-    x_g = xPupil*np.cos(theta) - yPupil*np.sin(theta)
-    y_g = xPupil*np.sin(theta) + yPupil*np.cos(theta)
-
-    # x_g and y_g are now the x and y coordinates
-    # can now use the PALPY method palDtp2s to convert to RA, Dec.
-
-    if are_arrays:
-        raObs, decObs = palpy.dtp2sVector(x_g, y_g, ra_pointing, dec_pointing)
-    else:
-        raObs, decObs = palpy.dtp2s(x_g, y_g, ra_pointing, dec_pointing)
-
-    return raObs, decObs
-
 
 if __name__ == "__main__":
 
